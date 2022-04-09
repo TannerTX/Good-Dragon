9d87837441c9297a5a84a53f98c0740d68e832f3
TannerTX <tanner_bibb@yahoo.com>
2022-04-09 01:03:08 -0500

    Animated register button, handling duplicate users

--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -6,16 +6,22 @@ import Axios from "axios"
 function Register() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [user, setUser] = useState("")
 
-    const register = () => {
-        Axios.post("http://localhost:3000/register", {
-        user: username,
-        pass: password
-        })
+    const register = async e => {
+        e.preventDefault()
+        const user = {username, password}
+        const response = await Axios.post("http://localhost:3001/register", user).then(
+            (response) => {console.log(response)}
+        )
+        
     }
 
+    
 
-
+    if (user) {
+        return <div>{user.name} is logged in.</div>
+    }
 
     return (
         
@@ -31,7 +37,7 @@ function Register() {
                 <h1>Sign-in</h1>
 
                 <form>
-                    <h5>E-mail</h5>
+                    <h5>Username</h5>
                     <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
 
                     <h5>Password</h5>
@@ -39,7 +45,12 @@ function Register() {
 
                     
                 </form>
-                <button onClick={register} className='login__registerButton'>Create Account</button>
+                <button class="cssbuttons-io-button" onClick={register}> Register
+                <div class="icon">
+                <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
+                </div>
+                </button>
+
                 <p>
                     By signing-in you agree to the <strong>Good Dragon</strong> Conditions of Use & Sale. Please
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
--- server/index.js
+++ server/index.js
@@ -1,27 +1,50 @@
 const express = require('express')
-const app = express();
 const mysql = require('mysql')
+const cors = require("cors")
+const passHash = require("password-hash")
+const port = 3001
 require('dotenv').config()
+
+const app = express();
+app.use(express.json())
+app.use(cors())
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: '3306',
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
+    database: 'GoodDragonDB'
+})
+
+db.connect()
+
+app.post("/register", (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+    console.log(`\n\nUSERNAME: ${username}\nPASSWORD: ${passHash.generate(password)}\nVERIFICATION: ${passHash.verify(password, passHash.generate(password))}`)
+    
+
+    if (!username || !password) {
+        console.log("INVALID USER OR PASS")
+    }
+    else {
+        db.query(`SELECT username FROM userLoginInfo WHERE username=?`, username, (err, result) => {
+            if(err) console.log(err)
+            
+            else if(result[0] != null)
+            console.log("USER ALREADY EXISTS")
+
+            else console.log("USER DOES NOT EXIST")
+        })
+
+        
+
+
+
+    }
 })
 
 
-app.get('/', (req, res) => {
-    res.send("Heyy World!")
-
-    db.connect(function (err) {
-        if (err) 
-        console.log(`Error connecting to database!\n${err}`);
-        else
-        console.log("Connected to the database!")
-    })
-
-})
-
-
-app.listen(3001, () => { console.log('listening on port 3001'); })
+app.listen(port, () => { console.log(`Listening on port ${port}`); })
