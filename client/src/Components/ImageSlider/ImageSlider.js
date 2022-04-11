d9ab63d56183a5b092e087c6cd59ff5cc6ded3a8
TannerTX <tanner_bibb@yahoo.com>
2022-04-11 18:56:57 -0500

    Fixing carousel again

--- client/src/Components/ImageSlider/ImageSlider.js
+++ client/src/Components/ImageSlider/ImageSlider.js
@@ -4,17 +4,15 @@ import '@splidejs/react-splide/css';
 import { RequestInterval } from '@splidejs/splide';
 
 function ImageSlider(props){
-
-        return (
-            <>
-            <Splide options={{rewind: true , autoplay: true, interval: 5000, pauseOnHover: true, padding: '30%',gap:'10%',speed:1500}}>
-                
+    
+        return (    
+            <Splide options={{rewind: true , autoplay: true, interval: 5000, pauseOnHover: true, padding: '30%',gap:'5%',speed:1500}}>
+        
                 {props.imgs.map((img) => 
                 <SplideSlide><img src={img} /></SplideSlide>
                 )}
                 
-            </Splide>              
-            </>
+            </Splide>                 
         )
     }
 export default ImageSlider;
