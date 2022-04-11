e30070b8803955fd461ed065586e409855aebe8b
TannerTX <tanner_bibb@yahoo.com>
2022-04-11 18:50:16 -0500

    Image slider works, fixing sizing

--- client/src/Components/ImageSlider/ImageSlider.js
+++ client/src/Components/ImageSlider/ImageSlider.js
@@ -1,14 +1,17 @@
+import React from "react"
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 import { RequestInterval } from '@splidejs/splide';
 
 function ImageSlider(props){
+
         return (
             <>
-            <Splide options={{rewind: true , autoplay: true, interval: 5000, pauseOnHover: true, padding: '20%',gap:'10%',speed:1500}}>
-                <SplideSlide>
-                    <img src={props.img1} height = "200" width = "100%" alt = "ERROR" />
-                </SplideSlide>
+            <Splide options={{rewind: true , autoplay: true, interval: 5000, pauseOnHover: true, padding: '30%',gap:'10%',speed:1500}}>
+                
+                {props.imgs.map((img) => 
+                <SplideSlide><img src={img} /></SplideSlide>
+                )}
                 
             </Splide>              
             </>
