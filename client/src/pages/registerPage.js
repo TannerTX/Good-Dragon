3e5e4b6679a16bf50f3e8b1f0fe2ceaf39a3d111
TannerTX <tanner_bibb@yahoo.com>
2022-04-12 01:35:51 -0500

    Added minor input validation on login/register

--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -8,21 +8,45 @@ function Register() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [formErrors, setFormErrors] = useState({})
     const [user, setUser] = useState("")
 
     
     const register = async e => {
         e.preventDefault()
+        
         const user = {username, password}
-        const response = await Axios.post("http://localhost:3001/register", user).then(
-            response => {console.log(response)})
+        setFormErrors(validate(user))
+
+        if(Object.values(formErrors).length === 0){
+        const response = await Axios.post("http://localhost:3001/register", user).then(response =>{console.log(response)})
+        }
+
         }
 
     const login = async e => {
         e.preventDefault()
+
         const user = {username, password}
+        setFormErrors(validate(user))
+        
+        if(Object.values(formErrors).length === 0){
         const response = await Axios.post("http://localhost:3001/login", user).then(response =>{console.log(response)})
         }
+
+        }
+
+    const validate = (obj) => {
+        const errors = {}
+        
+        if(!obj.username)
+        errors.username = "Username must not be empty!"
+
+        if(!obj.password)
+        errors.password = "Password must not be empty!"
+
+        return errors
+    }
 
     return (
         
@@ -39,21 +63,21 @@ function Register() {
 
                 <form>
                     <h5>Username</h5>
-                    <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
+                    <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder={formErrors.username} />
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />     
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder={formErrors.password} />  
                 </form>
 
 
-                <button class="cssbuttons-io-button" onClick={login}> Login
-                <div class="icon">
+                <button className="cssbuttons-io-button" onClick={login}> Login
+                <div className="icon">
                 <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
                 </div>
                 </button>
 
                 
-                <button class="cssbuttons-io-button" onClick={register}> Register
-                <div class="icon">
+                <button className="cssbuttons-io-button" onClick={register}> Register
+                <div className="icon">
                 <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
                 </div>
                 </button>
--- client/src/pages/testingPage.js
+++ client/src/pages/testingPage.js
@@ -2,17 +2,16 @@ import React from "react"
 import ImageSlider from "../Components/ImageSlider/ImageSlider.js";
 import "./testingPage.css"
 
-function Testing() {
-    
-    function importAll(r) {
+function importAll(r) {
         return r.keys().map(r);
       }
-      const images = importAll(require.context('../assets/images/', false, /\.(png|jpe?g|svg)$/));
+
+function Testing() {
+    
+    const images = importAll(require.context('../assets/images/', false, /\.(png|jpe?g|svg)$/));
     
     return (
-        
-        <ImageSlider imgs={images} />
-        
+        <ImageSlider imgs={images} />  
     )
 
 }
