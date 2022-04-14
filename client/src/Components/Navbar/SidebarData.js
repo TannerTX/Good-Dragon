e1b8f8abcaf63b575c32c65bde05061dae1fccd0
meiscoolman1 <stevensontyler461@gmail.com>
2022-04-13 20:44:50 -0500

    navbar messing around

--- client/src/Components/Navbar/SidebarData.js
+++ client/src/Components/Navbar/SidebarData.js
@@ -5,12 +5,14 @@ import * as IoIcons from 'react-icons/io';
 
 export const SidebarData = [
   {
+    class: 'current',
     title: 'Home',
     path: '/',
     icon: <AiIcons.AiFillHome />,
     cName: 'nav-text'
   },
   {
+    class: 'current',
     title: 'Shop',
     path: '/products',
     icon: <FaIcons.FaCartPlus />,
@@ -25,24 +27,28 @@ export const SidebarData = [
   },
   */
   {
+    class: 'current',
     title: 'Account',
     path: '/register',
     icon: <FaIcons.FaEnvelopeOpenText />,
     cName: 'nav-text'
   },
   {
+    class: 'current',
     title: 'Support',
     path: '/support',
     icon: <IoIcons.IoMdHelpCircle />,
     cName: 'nav-text'
   },
   {
+    class: 'current',
     title: 'TESTING GROUNDS',
     path: '/testing',
     icon: <FaIcons.FaBattleNet />,
     cName: 'nav-text'
   },
   {
+    class: 'current',
     title: 'DATA',
     path: '/data',
     icon: <FaIcons.FaDev />,
