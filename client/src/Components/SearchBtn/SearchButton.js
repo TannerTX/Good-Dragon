992addbc524ab2bdb7ebbdca2ebb0fb66cd98780
meiscoolman1 <stevensontyler461@gmail.com>
2022-04-17 14:37:10 -0500

    added searchButton top of site component, WIP

--- client/src/Components/SearchBtn/SearchButton.js
+++ client/src/Components/SearchBtn/SearchButton.js
@@ -5,11 +5,12 @@ import { FaSearch } from "react-icons/fa"
 function SearchBtn() {
     return(
         <div class="search-box fade-in-image">
-        <button class="btn-search"><FaSearch /></button>
-        <input type="text" class="input-search" placeholder="Type to Search..."/>
+         <button class="btn-search"><FaSearch /></button>
+         <input type="text" class="input-search" placeholder="Type to Search..."/>
         </div>
     );
-
 }
+
+
 
 export default SearchBtn;
