6e08d34dbdf7e8a0e4b0846b50c9fe8f56be060e
Davidk127 <davidkent0904@gmail.com>
2022-04-24 20:02:24 -0500

    admin page changes

--- client/src/pages/adminPage.js
+++ client/src/pages/adminPage.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "../assets/styles/adminPage.css"
+
 
 function Admin() {
 
     return (
-        <div>THIS IS THE ADMIN PAGE</div>
+        <div class = "box">
+            <div class = "itemBox">
+                    
+            </div>
+            <div class = "orderBox">
+                    
+            </div>
+            <div class = "userBox">
+                    
+            </div>
+            <button class = "submit">SUBMIT </button>
+            <label for="DC">DISCOUNT CODE:</label><br/>
+            <input type="text" id="fname" name="DC"/>
+
+
+        </div>
     );
 
 }
