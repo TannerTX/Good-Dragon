70ffad68c5f73567d628d64dce8ebe23c6896eb4
Davidk127 <davidkent0904@gmail.com>
2022-04-26 11:39:10 -0500

    more admin page css

--- client/src/pages/adminPage.js
+++ client/src/pages/adminPage.js
@@ -6,18 +6,55 @@ function Admin() {
 
     return (
         <div class = "box">
+
             <div class = "itemBox">
+
+                <label for="items">LIST OF ITEMS</label>
+                <label for="nameChange" class = "nameLabel">NAME</label>
+                <label for="priceChange" class = "priceLabel">PRICE</label>
+                <label for="descChange" class = "descLabel">DESCRIPTION</label>
+                <br/>
+
+                <select name="items" class = "itemDrop">
+                    <optgroup label = "ITEMS">
+                        <option value="placeholder">placeholders</option>
+                        <option value="item1">item 1</option>
+                        <option value="item2">item 2</option>
+                        <option value="item3">item 3</option>
+                        <option value="item4">item 4</option>
+                    </optgroup>
+                </select>
+ 
+                <input type="text" name="nameChange" class = "nameChange" />
+                <input type="text" name="priceChange" class = "priceChange" />
+                <input type="text" name="descChange" class = "descChange" />
+
+
+
+
                     
             </div>
+
             <div class = "orderBox">
+                <p> this is where the order query is gonna go</p>
                     
             </div>
+
             <div class = "userBox">
+                <label for="userSearch" class = "userLabel">USERNAME</label>
+                <label for="passChange" class = "passLabel"> CHANGE PASSWORD</label>
+                <br/>
+                <input type="text" name="userSearch" class = "userSearch" />
+                <input type="text" name="passChange" class = "passChange" />
                     
+                <div class = "displayUser">
+                    <p> this is where i want to display the user info from the search</p>
+                </div>
             </div>
+
             <button class = "submit">SUBMIT </button>
             <label for="DC">DISCOUNT CODE:</label><br/>
-            <input type="text" id="fname" name="DC"/>
+            <input type="text" name="DC" class = "discountCode" />
 
 
         </div>
