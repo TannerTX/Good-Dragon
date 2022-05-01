87f18ad7f3c0f9b5e4e0c47b6ea712d173c9fb9c
TannerTX <tanner_bibb@yahoo.com>
2022-04-30 23:29:47 -0500

    package

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors = require("cors")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const port = 3001, saltRounds = 10
 require('dotenv').config()
 
